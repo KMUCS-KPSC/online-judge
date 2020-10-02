1ca5ff5a01c4f4b3b0c11a605cad32a05337cdb3
Sang-geon Yun <c0510gy@protonmail.ch>
2020-10-02 22:42:23 +0900

    feature: add basic pages

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,10 @@ import {
   Switch,
 } from 'react-router-dom';
 import Landing from './pages/landing';
+import Problems from './pages/problems';
+import Contest from './pages/contest';
+import Rank from './pages/rank';
+import Discuss from './pages/discuss';
 import NavBar from './components/navBar';
 import React from 'react';
 import logo from './logo.svg';
@@ -12,9 +16,18 @@ import './App.css';
 function App() {
   return (
       <Router>
+        <NavBar/>
         <Switch>
+          <Route path='/problems'
+                 render={() => <div><Problems/></div>}/>
+          <Route path='/contest'
+                 render={() => <div><Contest/></div>}/>
+          <Route path='/rank'
+                 render={() => <div><Rank/></div>}/>
+          <Route path='/discuss'
+                 render={() => <div><Discuss/></div>}/>
           <Route path='/'
-                 render={() => <div><NavBar/><Landing/></div>}/>
+                 render={() => <div><Landing/></div>}/>
         </Switch>
       </Router>
   );
